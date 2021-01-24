d702e3f4a6ed8992163e1ebebd2e00515f3f0740
bill-yan-666 <wyan34@wisc.edu>
2021-01-23 20:00:39 -0600

    check

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb+srv://new-user-main:Kepleareleven11@@cluster0.hsnps.mongodb.net/inGrind?retryWrites=true&w=majority",
-  secretOrKey: "secret"
+    mongoURI: "mongodb+srv://Bill:Yx968606@cluster0.jh9df.mongodb.net/StudyLair?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
 //mongodb+srv://new-user-main:Kepleareleven11@@cluster0.hsnps.mongodb.net/inGrind?retryWrites=true&w=majority
 
